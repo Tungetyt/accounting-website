acea4c4a6689aca06a247895df69f046d93b7fd9
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-10-03 17:23:48 +0200

    Dark mode works

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,37 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import "../components/layout.css"
+import '../components/layout.css';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
-const IndexPage = () => (
+import { ThemeProvider } from '@material-ui/core';
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Brightness3Icon from '@material-ui/icons/Brightness3';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import SEO from '../components/seo';
+import Image from '../components/image';
+import Layout from '../components/layout';
+
+export const light = {
+  palette: {
+    type: 'light',
+  },
+};
+export const dark = {
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#b9f6ca',
+    },
+    secondary: {
+      main: '#b9f6ca',
+    },
+  },
+};
+
+const IndexPage = () => {
   // <Layout>
   //   <SEO title="Home" />
   //   <h1>Hi people</h1>
@@ -19,12 +43,28 @@ const IndexPage = () => (
   //   <Link to="/page-2/">Go to page 2</Link> <br />
   //   <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   // </Layout>
-  <>
-    <CssBaseline/>
-    <SEO title="Home" />
-    <Typography>AAAAAAAAAAAAAAAAAAAAAAA</Typography>
-    
-  </>
-)
 
-export default IndexPage
+  const [isDark, setTheme] = useState(true);
+  const icon = !isDark ? <Brightness3Icon /> : <Brightness7Icon />;
+  const appliedTheme = createMuiTheme(isDark ? dark : light);
+  return (
+
+    <>
+      <ThemeProvider theme={appliedTheme}>
+
+        <CssBaseline />
+        <SEO title="Home" />
+        <IconButton
+          edge="end"
+          color="inherit"
+          aria-label="mode"
+          onClick={() => setTheme((prevDark) => !prevDark)}
+        >
+          {icon}
+        </IconButton>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default IndexPage;
