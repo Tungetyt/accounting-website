77c253ea9284beb5b595d98b5ddd5f2c6705b005
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-10-10 10:44:44 +0200

    Refact

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,52 +29,6 @@ import ThemeBtn from '../components/theme-btn';
 import ScrollTop from '../components/scroll-top';
 import { getItemByKeyIfPossible, localStorageKey, langNames } from '../helpers';
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     position: 'fixed',
-//     bottom: theme.spacing(2),
-//     right: theme.spacing(2),
-//   },
-// }));
-
-// function ScrollTop(props) {
-//   const { children, window } = props;
-//   const classes = useStyles();
-//   // Note that you normally won't need to set the window ref as useScrollTrigger
-//   // will default to window.
-//   // This is only being set here because the demo is in an iframe.
-//   const trigger = useScrollTrigger({
-//     target: window ? window() : undefined,
-//     disableHysteresis: true,
-//     threshold: 100,
-//   });
-
-//   const handleClick = (event) => {
-//     const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
-
-//     if (anchor) {
-//       anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
-//     }
-//   };
-
-//   return (
-//     <Zoom in={trigger}>
-//       <div onClick={handleClick} role="presentation" className={classes.root}>
-//         {children}
-//       </div>
-//     </Zoom>
-//   );
-// }
-
-// ScrollTop.propTypes = {
-//   children: PropTypes.element.isRequired,
-//   /**
-//    * Injected by the documentation to work in an iframe.
-//    * You won't need it on your project.
-//    */
-//   window: PropTypes.func,
-// };
-
 const IndexPage = () => {
   const themeNames = {
     light: 'light',
@@ -109,11 +63,9 @@ const IndexPage = () => {
 
   const [isDark, setIsDark] = useState(
     true,
-    // getItemByKeyIfPossible(localStorageKey.theme) !== themeNames.light,
   );
   const [isPl, setIsPl] = useState(
     true,
-    // getItemByKeyIfPossible(localStorageKey.language) !== langNames.en,
   );
 
   useEffect(() => {
