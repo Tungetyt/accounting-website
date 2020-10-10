6fa38a78e47ec9e4e4492bd5b39dc9c98db43cb6
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-10-10 10:36:13 +0200

    Back to top

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,14 @@ import Brightness7Icon from '@material-ui/icons/Brightness7';
 import Tooltip from '@material-ui/core/Tooltip';
 import Zoom from '@material-ui/core/Zoom';
 import indigo from '@material-ui/core/colors/indigo';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import SEO from '../components/seo';
 import Image from '../components/image';
 import Layout from '../components/layout';
@@ -19,6 +27,52 @@ import LangBtn from '../components/lang-btn';
 import LangContext from '../context/lang-context';
 import ThemeBtn from '../components/theme-btn';
 import { getItemByKeyIfPossible, localStorageKey, langNames } from '../helpers';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = useStyles();
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
 
 const IndexPage = () => {
   const themeNames = {
@@ -73,8 +127,9 @@ const IndexPage = () => {
       <ThemeProvider theme={chosenTheme}>
         <CssBaseline />
         <SEO title="Vavicom" lang="pl" />
+        <div id="back-to-top-anchor" />
         <LangContext.Provider value={[isPl, setIsPl]}>
-          <Tooltip title={dict.themeBtn[isPl ? 'pl' : 'en']} arrow TransitionComponent={Zoom}>
+          <Tooltip title={dict.themeBtn[isPl ? 'pl' : 'en']} arrow TransitionComponent={Zoom} id="back-to-top-anchor">
             <IconButton
               edge="end"
               color="primary"
@@ -127,7 +182,13 @@ const IndexPage = () => {
           <Typography>aaaaaaaaaaaaaaaaa</Typography>
           <Typography>aaaaaaaaaaaaaaaaa</Typography>
           <Typography>aaaaaaaaaaaaaaaaa</Typography>
+          <ScrollTop>
+            <Fab color="secondary" size="small" aria-label="scroll back to top">
+              <KeyboardArrowUpIcon />
+            </Fab>
+          </ScrollTop>
         </LangContext.Provider>
+
       </ThemeProvider>
     </>
   );
