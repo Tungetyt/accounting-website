75e688d322a3352b5d25b2140b1d053f30fc741b
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-07 21:10:23 +0100

    clean

--- src/components/faq.js
+++ src/components/faq.js
@@ -22,8 +22,8 @@ export default function Faq() {
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
+
   const [isDark, setIsDark] = useContext(ColorContext);
-  // const { backgroundColor } = APP_THEME[isDark ? 'dark' : 'light'].palette.elevation2.backgroundColor;
 
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -50,7 +50,6 @@ export default function Faq() {
   } = useStyles();
   const { faq } = NAVIGATION;
   const intl = useIntl();
-  const faqCount = en.faqSection.length;
 
   return (
     <Element name={faq}>
@@ -69,88 +68,6 @@ export default function Faq() {
             </AccordionDetails>
           </Accordion>
         ))}
-
-        {/* {[...Array(faqCount)].map((x, i) => (
-          <Accordion className={accordion} expanded={expanded === i} onChange={handleChange(i)}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-            >
-              <Typography align="justify" style={{ maxWidth: '600px' }}>{intl.formatMessage({ id: `${obj.q}` })}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                <Typography align="justify" style={{ maxWidth: '600px' }}>{intl.formatMessage({ id: `${obj.a}` })}</Typography>
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        ))} */}
-        {/* {en.faqSection.map((obj, i) => (
-
-          <Accordion style={{ backgroundColor: APP_THEME[isDark ? 'dark' : 'light'].palette.elevation2.backgroundColor }} expanded={expanded === i} onChange={handleChange(i)}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-
-            >
-              <Typography align="justify" style={{ maxWidth: '600px' }}>{intl.formatMessage({ id: `${obj.q}` })}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                <Typography align="justify" style={{ maxWidth: '600px' }}>{intl.formatMessage({ id: `${obj.a}` })}</Typography>
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        ))} */}
-        {/* <Accordion className={accordion} expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2bh-content"
-            id="panel2bh-header"
-          >
-            <Typography className={heading}>Users</Typography>
-            <Typography className={secondaryHeading}>
-              You are currently not an owner
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
-              diam eros in elit. Pellentesque convallis laoreet laoreet.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion className={accordion} expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3bh-content"
-            id="panel3bh-header"
-          >
-            <Typography className={heading}>Advanced settings</Typography>
-            <Typography className={secondaryHeading}>
-              Filtering has been entirely disabled for whole web server
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-              vitae egestas augue. Duis vel est augue.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion className={accordion} expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel4bh-content"
-            id="panel4bh-header"
-          >
-            <Typography className={heading}>Personal data</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-              vitae egestas augue. Duis vel est augue.
-            </Typography>
-          </AccordionDetails>
-        </Accordion> */}
       </div>
     </Element>
   );
