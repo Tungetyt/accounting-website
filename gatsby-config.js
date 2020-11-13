6422e4d1042065054b9600d842e2d7ea01fb0152
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-13 16:14:02 +0100

    meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Biuro Rachunkowe',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    description: 'Biuro Rachunkowe Vavicom Józefosław Warszawa Piaseczno',
+    author: '@tungetyt',
   },
   plugins: [
     {
