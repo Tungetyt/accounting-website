3bbd8532994921a6be80410e53a1030809fdfa5e
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-13 12:15:04 +0100

    contact alignment fix

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,20 +1,16 @@
 /* eslint-disable comma-dangle */
 import {
+  Box,
   Grid, Hidden
 } from '@material-ui/core';
-import { useIntl } from 'gatsby-plugin-intl';
-import React, { useContext } from 'react';
+import React from 'react';
 import { Element } from 'react-scroll';
-import { ColorContext } from '../context/contexts';
 import { NAVIGATION } from '../helpers';
 import ContactInfo from './contact-info';
 import OpenMap from './open-map';
 
 const Contact = () => {
-  const intl = useIntl();
-
   const { contact } = NAVIGATION;
-  const [isDark] = useContext(ColorContext);
   return (
     <Element name={contact}>
       <Grid
@@ -23,11 +19,13 @@ const Contact = () => {
         justify="center"
         alignItems="center"
       >
-        <Grid item style={{ marginRight: '4rem', marginBottom: '2rem' }}>
+        <Grid item>
           {typeof window !== 'undefined' && (
           <>
             <Hidden smDown implementation="css">
-              <OpenMap />
+              <Box style={{ marginRight: '4rem', marginBottom: '2rem' }}>
+                <OpenMap />
+              </Box>
             </Hidden>
           </>
           )}
