6069ff5e610cf4bb2664ddedd64eb64b09855218
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-16 15:25:56 +0100

    reverted last commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,5 @@
 module.exports = {
   siteMetadata: {
-    title: 'BIURO RACHUNKOWE VAVICOM PIASECZNO JÓZEFOSŁAW WARSZAWA',
     description: 'Biuro Rachunkowe VAVICOM Piaseczno, Józefosław, Warszawa. Oferujemy usługi księgowości, obsługę kadr i płac oraz doradztwo podatkowe, finansowe i prawne dla firm z całej Polski. Zapewniamy profesjonalną obsługę podmiotów gospodarczych osób fizycznych. Proponujemy konkurencyjne ceny. Pomagamy założyć działalność gospodarczą i firmę.',
     author: '@tungetyt',
   },
