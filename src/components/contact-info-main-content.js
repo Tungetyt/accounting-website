694309bfce1fdf33a181096ce40e60ee8f23d1b7
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-18 13:12:47 +0100

    contact additional info

--- src/components/contact-info-main-content.js
+++ src/components/contact-info-main-content.js
@@ -3,6 +3,7 @@ import {
   Grid, Typography
 } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 import BusinessIcon from '@material-ui/icons/Business';
 import EmailIcon from '@material-ui/icons/Email';
 import PhoneIcon from '@material-ui/icons/Phone';
@@ -10,13 +11,20 @@ import { useIntl } from 'gatsby-plugin-intl';
 import React, { useContext } from 'react';
 import { ColorContext } from '../context/contexts';
 import {
-  APP_THEME, EMAIL, EMAIL2, PHONE_NUMBER
+  APP_THEME, EMAIL, EMAIL2, PHONE_NUMBER, PHONE_NUMBER2
 } from '../helpers';
 
+const useStyles = makeStyles(() => ({
+
+  typoSpace: {
+    marginBottom: '0.4rem'
+  },
+}));
 const ContactInfoMainContent = () => {
   const intl = useIntl();
   const [isDark] = useContext(ColorContext);
   const color = isDark ? '#FFFFFF' : 'primary';
+  const { typoSpace } = useStyles();
   return (
     <Paper style={{ backgroundColor: APP_THEME[isDark ? 'dark' : 'light'].palette.elevation2.backgroundColor, padding: '1rem' }}>
       <Grid container direction="column">
@@ -32,14 +40,20 @@ const ContactInfoMainContent = () => {
               <BusinessIcon color={color} />
             </Grid>
             <Grid item>
-              <Typography color={color}>
+              <Typography color={color} className={typoSpace}>
                 {intl.formatMessage({ id: 'contactSection.name' })}
               </Typography>
               <Typography color={color}>
                 {intl.formatMessage({ id: 'contactSection.street' })}
               </Typography>
-              <Typography color={color}>
+              <Typography color={color} className={typoSpace}>
                 {intl.formatMessage({ id: 'contactSection.town' })}
+              </Typography>
+              <Typography color={color}>
+                {intl.formatMessage({ id: 'contactSection.street2' })}
+              </Typography>
+              <Typography color={color}>
+                {intl.formatMessage({ id: 'contactSection.town2' })}
               </Typography>
             </Grid>
           </Grid>
@@ -58,6 +72,9 @@ const ContactInfoMainContent = () => {
             <Grid item>
               <Typography color={color}>
                 {PHONE_NUMBER}
+              </Typography>
+              <Typography color={color}>
+                {PHONE_NUMBER2}
               </Typography>
             </Grid>
           </Grid>
