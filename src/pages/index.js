4da3281e006d457f04c027d6a4ae3c5259848330
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-19 10:13:37 +0100

    title too long

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ import TabPanel from '../components/tab-panel';
 import { ColorContext } from '../context/contexts';
 import {
   // eslint-disable-next-line comma-dangle
-  APP_THEME, DESCRIPTION, DRAWER_WIDTH, getItemByKey, LOCAL_STORAGE_KEY, THEME_NAMES, TITLE
+  APP_THEME, DESCRIPTION, DRAWER_WIDTH, getItemByKey, LOCAL_STORAGE_KEY, THEME_NAMES
 } from '../helpers';
 
 const IndexPage = (props) => {
@@ -52,7 +52,7 @@ const IndexPage = (props) => {
       <ThemeProvider theme={chosenTheme}>
         <ColorContext.Provider value={[isDark, setIsDark]}>
           <CssBaseline />
-          <SEO title={TITLE} description={DESCRIPTION} />
+          <SEO description={DESCRIPTION} />
           <Logo />
           <ResponsiveDrawer props={props} />
           <div id="top" />
