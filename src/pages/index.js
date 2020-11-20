1f119812ce47b99b0f7e6ddebf2213649b716532
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-21 00:27:24 +0100

    title fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ const IndexPage = (props) => {
       <ThemeProvider theme={chosenTheme}>
         <ColorContext.Provider value={[isDark, setIsDark]}>
           <CssBaseline />
-          <SEO title="biuro rachunkowe" description={DESCRIPTION} />
+          <SEO title="Biuro rachunkowe" description={DESCRIPTION} />
           <Logo />
           <ResponsiveDrawer props={props} />
           <div id="top" />
