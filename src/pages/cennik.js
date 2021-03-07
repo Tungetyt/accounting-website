f8378da6f4dfcd24ed931408a767841bccede6da
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2021-03-07 17:55:34 +0100

    SubPages

---
+++ src/pages/cennik.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Prices from '../components/prices';
+import Layout from '../components/layout';
+import '../components/layout.css';
+
+const Ceny = () => (
+  <Layout>
+    <Prices />
+  </Layout>
+);
+
+export default Ceny;
---
+++ src/pages/uslugi.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TabPanel from '../components/tab-panel';
+import Layout from '../components/layout';
+import '../components/layout.css';
+
+const Services = () => (
+  <Layout>
+    <TabPanel />
+  </Layout>
+);
+
+export default Services;
